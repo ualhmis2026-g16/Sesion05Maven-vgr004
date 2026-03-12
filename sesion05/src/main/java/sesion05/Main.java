50b2e71608005c8ba67be80e181eb4ef5ff2bdd6
Victor Granero Reche <vgr004@inlumine.ual.es>
2026-03-12 14:22:58 +0100

    Primer commit

---
+++ sesion05/src/main/java/sesion05/Main.java
@@ -0,0 +1,20 @@
+package sesion05;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+    	String ruta = "data/coches.json";
+    	
+    	Coche[] coches = JsonReader.leerCochesJSON(ruta);
+    	
+    	// Imprimir los objetos del arreglo
+        for (Coche coche : coches) {
+            System.out.println(coche);
+        }
+    }
+}
