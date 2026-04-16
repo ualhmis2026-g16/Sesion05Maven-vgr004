29402965b0ac68588472bc1cbeaf16dfaab8fc0e
Victor Granero Reche <vgr004@inlumine.ual.es>
2026-04-16 12:53:39 +0200

    UML

--- sesion05/target/site/es/testapidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"sesion05","c":"JsonReaderTest","l":"JsonReaderTest()","u":"%3Cinit%3E()","k":"3"},{"p":"sesion05","c":"JsonReaderTest","l":"testLeerCochesJSON()"},{"p":"sesion05","c":"JsonReaderTest","l":"testLeerCochesJSONprimero()"}];updateSearchResults();
